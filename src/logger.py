e6aa28cf9866fa782c5e743fb93608a0fd6976d1
PabloDavidAR <pablo.arandarod@gmail.com>
2023-07-14 11:12:12 +0200

    Initial commit for logger

---
+++ src/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+class HomeFormatter(logging.Formatter):
+    def __init__(self):
+        pass
+
+class HomeLogger(logging.getLoggerClass()):
+    def __init__(self):
+        pass
+
